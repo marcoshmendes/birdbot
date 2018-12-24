e29682cca313c0dfc017dd99ec9163245268e71f
Marcos Mendes <marcos.mendes@live.com>
2018-12-24 02:47:22 -0300

    change search to POST

--- app/route.js
+++ app/route.js
@@ -3,6 +3,6 @@ const controller = require('./controller');
 module.exports = function(app) {
     app.route('/api/retweet').post(controller.retweet);
     app.route('/api/like').post(controller.like);
-    app.route('/api/search').get(controller.search);
+    app.route('/api/search').post(controller.search);
     app.route('/api/tweet').post(controller.tweet);
 };
